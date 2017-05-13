b93f104d2d12914112f7e4ddcba79795679c0504
Komphet <komphet@kmi.tl>
2017-05-13 18:55:18 +0700

    Update cover image

--- js/scripts.js
+++ js/scripts.js
@@ -110,7 +110,7 @@
 		
         $(window).load(function(){
         $(".typing").typed({
-            strings: ["I am A Freelancer.", "I Design Websites.", "I Live in New York."],    /* You can change the home section typing text from
+            strings: ["I am A Freelancer.", "I Design Websites.", "I'm a student of KMITL.", "I Live in Bangkok, Thailand."],    /* You can change the home section typing text from
 	                                                                                            here and do not use "&" use "and" */
             typeSpeed: 50
           });
